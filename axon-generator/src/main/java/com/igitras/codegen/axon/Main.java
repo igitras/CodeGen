096c8ab9283343ed01fa159a55787ca4e062fd8e
Mason Mei <dongxu.m@gmail.com>
2015-01-05 00:02:59 +0800

    Add new interface

---
+++ axon-generator/src/main/java/com/igitras/codegen/axon/Main.java
@@ -0,0 +1,6 @@
+package com.igitras.codegen.axon;
+
+/**
+ * Created by mason on 12/26/14.
+ */
+public class Main {}
---
+++ common/src/main/java/com/igitras/codegen/common/next/CgTypeParameter.java
@@ -0,0 +1,6 @@
+package com.igitras.codegen.common.next;
+
+/**
+ * Created by mason on 1/4/15.
+ */
+public interface CgTypeParameter {}
